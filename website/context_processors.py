da556a54892a8306ad1d64313c0877b04b36c773
Vetle E <vetleengesbak@gmail.com>
2020-07-30 14:02:49 +0200

    small bug

--- website/context_processors.py
+++ website/context_processors.py
@@ -2,9 +2,7 @@ from django.conf import settings
 from django.http import HttpRequest
  
 #make sure we have the GA_KEY variable available in all templates 
-def google_analytics(request: HttpRequest):
-    print("hello context")
- 
+def google_analytics(request: HttpRequest): 
     return {
         'GA_KEY': settings.GOOGLE_ANALYTICS_KEY,
     }
